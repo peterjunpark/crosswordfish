a872f92101af89b7833024fea7d674534d365990
Peter Jun Park <park@hey.com>
2023-12-09 02:43:48 -0800

    chore: add another test puzzle

---
+++ src/___tests___/test-data/15x15-09-12-2023-washington-post/grid.ts
@@ -0,0 +1,20 @@
+import type { Grid } from "@/lib/types";
+
+// prettier-ignore
+export const testGrid3: Grid = [
+  [null, null, "G", "L", "O", "B", "A", "L", null, null, null, "T", "E", "R", "M"],
+  [null, "C", "O", "O", "L", "E", "D", "I", "T", null, "S", "O", "P", "U", "P"],
+  ["B", "L", "E", "N", "D", "E", "D", "F", "A", "M", "I", "L", "I", "E", "S"],
+  ["L", "O", "S", "E", "S", null, null, "E", "R", "O", "D", "E", "S", null, null],
+  ["O", "V", "A", null, null, "S", "N", "I", "P", "P", "E", "D", "O", "F", "F"],
+  ["G", "E", "T", "A", "R", "O", "O", "M", null, null, "A", "O", "D", "A", "I"],
+  [null, null, null, "R", "A", "B", "B", "I", "T", "S", null, null, "E", "R", "R"],
+  ["F", "O", "R", "R", "E", "A", "L", "T", "H", "I", "S", "T", "I", "M", "E"],
+  ["O", "B", "E", null, null, "D", "E", "A", "R", "G", "O", "D", null, null, null],
+  ["A", "V", "A", "I", "L", null, null, "T", "E", "N", "D", "S", "B", "A", "R"],
+  ["M", "I", "C", "R", "O", "B", "R", "E", "W", "S", null, null, "O", "B", "O"],
+  [null, null, "H", "A", "R", "R", "I", "S", null, null, "A", "S", "S", "E", "T"],
+  ["A", "V", "O", "I", "D", "A", "L", "L", "C", "O", "S", "T", "S"],
+  [],
+  ["A", "N", "T", "E", null, null, null, "T", "E", "N", "T", "E", "D", null, null],
+];
