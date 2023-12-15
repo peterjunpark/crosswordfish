964dcd0573c70bbe6bd0a60921418ed6899e9848
Peter Jun Park <park@hey.com>
2023-12-15 03:05:21 -0800

    chore: add freeform sample puzzle and rename test data

---
+++ src/dataset/sample-puzzles/index.ts
@@ -0,0 +1,11 @@
+import americanSample110623 from "./american/washington-post-110623.json";
+import americanSample112223 from "./american/washington-post-112223.json";
+import americanSample120923 from "./american/washington-post-120923.json";
+import freeformSampleTgif from "./freeform/tgif.json";
+
+export {
+  americanSample110623,
+  americanSample112223,
+  americanSample120923,
+  freeformSampleTgif,
+};
